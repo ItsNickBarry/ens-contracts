8da2a8e0867c95aeec67aba9565052f35701bbe3
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-22 14:23:45 -0600

    comment out compile task override to avoid artifact issue

--- tasks/esm_fix.ts
+++ tasks/esm_fix.ts
@@ -7,14 +7,15 @@ export default overrideTask('compile')
   .setAction(async (args, { config }, runSuper) => {
     const superRes = await runSuper(args)
 
-    try {
-      await writeFile(
-        path.join(config.paths.artifacts, 'package.json'),
-        '{ "type": "commonjs" }',
-      )
-    } catch (error) {
-      console.error('Error writing package.json: ', error)
-    }
+    // TODO: HH is treating this file as an artifact, which breaks plugins
+    // try {
+    //   await writeFile(
+    //     path.join(config.paths.artifacts, 'package.json'),
+    //     '{ "type": "commonjs" }',
+    //   )
+    // } catch (error) {
+    //   console.error('Error writing package.json: ', error)
+    // }
 
     return superRes
   })
